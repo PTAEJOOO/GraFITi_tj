c751dccfbd14a2456c3e7f6df4f38989c912645f
PTAEJOOO <qkrxown5985@gmail.com>
2024-11-26 01:57:48 +0900

    scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,22 +1,24 @@
 ### grafiti ###
 
-nlayers="1 2 3 4"
-attn_head="1 2 4"
-latent_dim="16 32 64"
-# 128 256
+# nlayers="4"
+# latent_dim="256"
+nlayers="4"
+attn_head="2 4"
+latent_dim="64 128"
 
-# for nlayer in $nlayers; do
-#     for at in $attn_head; do
-#         for ld in $latent_dim; do
-#             python train_grafiti.py \
-#             --epochs 200 --learn-rate 0.001 --batch-size 128 \
-#             --attn-head $at --latent-dim $ld --nlayers $nlayer \
-#             --dataset physionet2012 --fold 0 -ct 36 -ft 12 -wocat
-#         done
-#     done   
-# done
 
-python train_grafiti.py --epochs 100 --learn-rate 0.001 --batch-size 32 --dataset physionet2012 --fold 0 -ct 36 -ft 12
+for nlayer in $nlayers; do
+    for at in $attn_head; do
+        for ld in $latent_dim; do
+            python train_grafiti.py \
+            --epochs 20 --learn-rate 0.001 --batch-size 128 \
+            --attn-head $at --latent-dim $ld --nlayers $nlayer \
+            --dataset physionet2012 --fold 0 -ct 36 -ft 12 -wocat
+        done
+    done   
+done
+
+# python train_grafiti.py --epochs 100 --learn-rate 0.001 --batch-size 32 --dataset physionet2012 --fold 0 -ct 36 -ft 12
 
 # do
 #     python train_grafiti.py \
