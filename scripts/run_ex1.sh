d1f4bab6a53b18e5925fcfec2349705e1fe250cc
PTAEJOOO <qkrxown5985@gmail.com>
2024-11-28 01:05:27 +0900

    1127

--- scripts/run_ex1.sh
+++ scripts/run_ex1.sh
@@ -1,4 +1,4 @@
-### grafiti ###
+### ex1.py ###
 
 nlayers="1"
 attn_head="1"
@@ -7,7 +7,7 @@ latent_dim="16"
 for nlayer in $nlayers; do
     for at in $attn_head; do
         for ld in $latent_dim; do
-            python train_grafiti.py \
+            python ex1.py \
             --epochs 200 --learn-rate 0.001 --batch-size 128 \
             --attn-head $at --latent-dim $ld --nlayers $nlayer \
             --dataset physionet2012 --fold 0 -ct 36 -ft 12
