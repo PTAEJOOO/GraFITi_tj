ade2303b330cf7caa0f384f18716e101719fd502
PTAEJOOO <qkrxown5985@gmail.com>
2024-11-27 15:13:21 +0900

    eval

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,8 +1,8 @@
 ### grafiti ###
 
-nlayers="2"
+nlayers="4"
 attn_head="2 4"
-latent_dim="64 128"
+latent_dim="64"
 
 for nlayer in $nlayers; do
     for at in $attn_head; do
@@ -10,7 +10,7 @@ for nlayer in $nlayers; do
             python eval_grafiti.py \
             --epochs 200 --learn-rate 0.001 --batch-size 128 \
             --attn-head $at --latent-dim $ld --nlayers $nlayer \
-            --dataset physionet2012 --fold 0 -ct 36 -ft 12 -wocat -ax
+            --dataset physionet2012 --fold 0 -ct 36 -ft 12 -wocat
         done
     done   
 done
