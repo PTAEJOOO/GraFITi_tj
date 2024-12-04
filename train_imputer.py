754d6f544019e245f2ab7dd997abec9d6c9c3346
PTAEJOOO <qkrxown5985@gmail.com>
2024-12-04 19:17:03 +0900

    change save name

--- train_imputer.py
+++ train_imputer.py
@@ -201,7 +201,7 @@ for batch in tqdm(TRAIN_LOADER):
     indicating_mask = np.isnan(X.cpu().numpy()) ^ np.isnan(X_ori.cpu().numpy())  # indicating mask for imputation error calculation
     mae = calc_mae(imputation, np.nan_to_num(X_ori.cpu().numpy()), indicating_mask)  # calculate mean absolute error on the ground truth (artificially-missing values)
     print(mae)
-    saits.save("saved_imputer/saits_physionet2012_ep100.pypots")  # save the model for future use
+    saits.save("saved_imputer/saits_physionet2012_0.3_ep100.pypots")  # save the model for future use
 
     ## loading
     # load_saits = SAITS(n_steps=37, n_features=37, n_layers=2, d_model=256, n_heads=4, d_k=64, d_v=64, d_ffn=128, dropout=0.1)
@@ -212,7 +212,7 @@ for batch in tqdm(TRAIN_LOADER):
     # mae = calc_mae(imputation, np.nan_to_num(X_ori.cpu().numpy()), indicating_mask)
     # print(mae)
     
-    sys.exit(0)
+sys.exit(0)
 ######################################################################################
 
 # def MSE(y: Tensor, yhat: Tensor, mask: Tensor) -> Tensor:
