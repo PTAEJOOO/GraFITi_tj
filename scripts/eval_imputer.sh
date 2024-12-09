9470baf2620b0fd61558f3591a17b5d353709485
PTAEJOOO <qkrxown5985@gmail.com>
2024-12-10 00:22:41 +0900

    script

--- scripts/eval_imputer.sh
+++ scripts/eval_imputer.sh
@@ -8,22 +8,7 @@ for nlayer in $nlayers; do
     for at in $attn_head; do
         for ld in $latent_dim; do
             python eval_grafiti.py \
-            --epochs 100 --learn-rate 0.001 --batch-size 128 \
-            --attn-head $at --latent-dim $ld --nlayers $nlayer \
-            --dataset physionet2012 --fold 0 -ct 36 -ft 12 -wocat
-        done
-    done   
-done
-
-nlayers="4"
-attn_head="2"
-latent_dim="64"
-
-for nlayer in $nlayers; do
-    for at in $attn_head; do
-        for ld in $latent_dim; do
-            python eval_grafiti.py \
-            --epochs 100 --learn-rate 0.001 --batch-size 128 \
+            --epochs 200 --learn-rate 0.001 --batch-size 128 \
             --attn-head $at --latent-dim $ld --nlayers $nlayer \
             --dataset physionet2012 --fold 0 -ct 36 -ft 12 -wocat
         done
