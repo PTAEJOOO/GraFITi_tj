8d9123182e0c620a273bd5c2979b8a1e8007c318
PTAEJOOO <qkrxown5985@gmail.com>
2024-12-09 19:36:22 +0900

    for mc sampling

--- scripts/run_imputer.sh
+++ scripts/run_imputer.sh
@@ -1,7 +1,22 @@
 ### train_grafiti_impute.py ###
 
-# nlayers="4"
-# attn_head="4"
+nlayers="2"
+attn_head="2"
+latent_dim="64"
+
+for nlayer in $nlayers; do
+    for at in $attn_head; do
+        for ld in $latent_dim; do
+            python train_grafiti_impute.py \
+            --epochs 200 --learn-rate 0.001 --batch-size 128 \
+            --attn-head $at --latent-dim $ld --nlayers $nlayer \
+            --dataset physionet2012 --fold 0 -ct 36 -ft 12 -wocat
+        done
+    done   
+done
+
+# nlayers="2"
+# attn_head="2 4"
 # latent_dim="256"
 
 # for nlayer in $nlayers; do
@@ -14,21 +29,6 @@
 #         done
 #     done   
 # done
-
-nlayers="2"
-attn_head="2 4"
-latent_dim="256"
-
-for nlayer in $nlayers; do
-    for at in $attn_head; do
-        for ld in $latent_dim; do
-            python train_grafiti_impute.py \
-            --epochs 200 --learn-rate 0.001 --batch-size 128 \
-            --attn-head $at --latent-dim $ld --nlayers $nlayer \
-            --dataset physionet2012 --fold 0 -ct 36 -ft 12 -wocat
-        done
-    done   
-done
 
 # nlayers="4"
 # attn_head="2"
